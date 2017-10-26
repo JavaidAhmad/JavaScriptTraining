dec6dc28208fe565be931147116adfddf21deb3d
JavaidAhmad <33113461+JavaidAhmad@users.noreply.github.com>
2017-10-26 15:27:00 +0530

    Add files via upload

---
+++ ResumeAssignment1.js
@@ -0,0 +1,59 @@
+var resume = {
+	"Objective": "To work in challanging environment where I can exhibit my utmost skills for the growth of organization along with me",
+	"Personal" : {
+		"name" : "Javaid",
+		"age" :  "23",
+		"gender" : "male",
+		"contact" : "9872290135",
+		"email"   : "javaidahmad.ganaie@clicklabs.co"
+	},
+	"Acadmics" : {
+		"institute" : {
+			"LPU" : {
+				"Course" : "MCA",
+				"CGPA"   : "9.34",
+				"Year"	 : "2018"	
+			} ,
+			"SITE": {
+				"Course" : "BCA",
+				"CGPA"   : "9.30",
+				"Year"	 : "2015"
+			},
+			"BOSE": {
+				"Course" : "12th",
+				"CGPA"	 : "7.9",
+				"Year"	 : "2012"
+			}
+		}
+	},
+	"Experience" : {
+		"Company"     : "Calance Software",
+		"Designation" : "Consultant Technology",
+		"Duration"	  : "1 year"
+	},
+	"Technical_Skills":{
+		"Programming_Technologies" : {
+			1 : "Java",
+			2 : "Swift",
+			3 : "PHP"
+		},
+		"Web_Technologies" :{
+			1 : "HTML",
+			2 : "CSS",
+			3 : "Jquery",
+			4 : "Bootstrap",
+			5 : "Java Script"
+		},
+		"QA_Tools" : {
+			1 : "Selenium webdriver",
+			2 : "testNG",
+			3 : "JUnit"
+		}
+	},
+	"Hobbies" : {
+		1 : "Cooking",
+		2 : "Playing chess and cricket",
+		3 : "Gymming"
+	}
+
+};
